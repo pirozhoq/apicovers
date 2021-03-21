0144bdb1f29dc9b764029885a1f154c015e943bd
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-22 01:42:48 +0200

    update config files

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,27 @@
+// module.exports = ({ env }) => ({
+// 	upload: {
+// 		provider: 'aws-s3',
+// 		providerOptions: {
+// 			accessKeyId: 'AKIAUNKULL3NRDH53RXT',
+// 			secretAccessKey: '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',
+// 			region: 'eu-central-1',
+// 			params: {
+// 				Bucket: 'strapi-apicovers',
+// 			},
+// 		},
+// 	},
+// });
+
 module.exports = ({ env }) => ({
-	upload: {
-		provider: 'aws-s3',
-		providerOptions: {
-			accessKeyId: 'AKIAUNKULL3NRDH53RXT',
-			secretAccessKey: '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',
-			region: 'eu-central-1',
-			params: {
-				Bucket: 'strapi-apicovers',
-			},
-		},
-	},
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET_NAME'),
+      },
+    },
+  },
 });
