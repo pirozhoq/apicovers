1f37ed279f329edd6dcfb4775a4ee6fe4f670f66
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-22 13:35:08 +0200

    disable conf/env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f26eb7ea8cee8711d20c176ed29f9902'),
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', 'f26eb7ea8cee8711d20c176ed29f9902'),
+//     },
+//   },
+// });
