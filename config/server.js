5ba3fbef2ee14d28b535fcaf39ba972acaf52b63
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-26 23:14:27 +0200

    server config

--- config/server.js
+++ config/server.js
@@ -1,14 +1,9 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   admin: {
-//     auth: {
-//       secret: env('ADMIN_JWT_SECRET', 'f26eb7ea8cee8711d20c176ed29f9902'),
-//     },
-//   },
-// });
-
-// module.exports = ({ env }) => ({
-// 	host: env('APP_HOST'),
-// 	port: env.int('NODE_PORT'),
-// });
+module.exports = ({ env }) => ({
+  host: env('HOST'),
+  port: env.int('PORT'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+});
