e025f44981e75be2ad91bb274f554cb8f2646418
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-27 00:01:39 +0200

    config

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,3 @@
-// module.exports = ({ env }) => ({
-// 	upload: {
-// 		provider: 'aws-s3',
-// 		providerOptions: {
-// 			accessKeyId: 'AKIAUNKULL3NRDH53RXT',
-// 			secretAccessKey: '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',
-// 			region: 'eu-central-1',
-// 			params: {
-// 				Bucket: 'strapi-apicovers',
-// 			},
-// 		},
-// 	},
-// });
-
 module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
