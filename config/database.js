085e90780150754151b714a0094c47a44baeada6
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-28 15:08:06 +0300

    fix database config ssl

--- config/database.js
+++ config/database.js
@@ -10,10 +10,8 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-      },
-      options: {
         ssl: false
-      },
+      }
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST'),
   port: env.int('PORT'),
-  url: 'http://18.195.97.9',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
