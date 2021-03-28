d32bdaf303a3a3348d81d0b0a6720c14b65ed99c
Vassilyev Anton <a.vasiljev.n@gmail.com>
2021-03-28 19:03:33 +0300

    config

--- config/database.js
+++ config/database.js
@@ -9,9 +9,11 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: false
-      }
+        password: env('DATABASE_PASSWORD')
+      },
+      options: {
+        ssl: false,
+      },
     },
   },
 });
